6d494d94d16299615b28436aef641e04fee57a31
Souman Paul <souman1729@gmail.com>
2019-12-24 15:19:07 +0530

    updated uri

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 // Load env vars
-dotenv.config({ path: './config.env'})
-// const uri = "mongodb://localhost:27017/SOUMAN"
+dotenv.config({ path: './config/config.env'})
+const uri = process.env.MONGO_URI
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(uri, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
